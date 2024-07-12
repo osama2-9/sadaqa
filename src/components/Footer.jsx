4fcc8531d8238b2fac7add84c569617d71ff7d6d
osama <osamaalsrraj3@gmail.com>
2024-07-12 17:13:25 +0300

    save

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,58 @@
-import { Box, Flex, Image, Link } from '@chakra-ui/react'
-0
+import { useState } from 'react';
+import { Box, Flex, Image, Link, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Text } from '@chakra-ui/react';
+
 const Footer = () => {
-  return (
-    <Box w={'full'} bg={'gray.50'}>
-        <Flex flexDir={'column'} justifyContent={'center'} alignItems={'center'}>
-            <Image src='/palestineFlag.png' />
+    const [isOpen, setIsOpen] = useState(false);
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
 
+    const steps = [
+        { src: '1.png', title: 'Click share button' },
+        { src: '2.png', title: 'Click add to home screen' },
+        { src: '3.png', title: 'Click add' },
+        { src: '4.png', title: 'The website will be saved as a bookmark' },
+    ];
 
-            <Link position={'relative'} top={'-70px'} textDecoration={'underline'} zIndex={10} color={'black'}>How to Save on your phone</Link>
+    return (
+        <Box w={'full'} bg={'gray.50'}>
+            <Flex flexDir={'column'} justifyContent={'center'} alignItems={'center'}>
+                <Image src='/palestineFlag.png' />
 
+                <Link
+                    position={'relative'}
+                    top={'-70px'}
+                    textDecoration={'underline'}
+                    zIndex={10}
+                    color={'black'}
+                    onClick={openModal}
+                >
+                    How to Save on your phone
+                </Link>
 
-        </Flex>
-      
-    </Box>
-  )
-}
+                <Modal isOpen={isOpen} onClose={closeModal}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>How to Save on your phone</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody bg={'gray.50'}>
+                            {steps.map((step, index) => (
+                                <Box key={index} mb={4}>
+                                    <Text textAlign={'center'} fontWeight={'bold'} mb={5} color={'black'}>{step.title}</Text>
+                                    <Image src={step.src} alt={step.title} mb={2} />
+                                </Box>
+                            ))}
+                        </ModalBody>
 
-export default Footer
+                        <ModalFooter>
+                            <Button colorScheme='blue' mr={3} onClick={closeModal}>
+                                Close
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </Flex>
+        </Box>
+    );
+};
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
-import { Box, Flex, Image, Text, useDisclosure, IconButton, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, VStack } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 
 const Header = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <Box color={'black'} shadow={'md'} w={'full'} h={'65px'}>
@@ -11,7 +9,7 @@ const Header = () => {
                     <Image src="/logo.png" w={'45px'} />
                 </Box>
 
-                <Flex display={{ base: 'none', md: 'flex' }} gap={5} me={'22'} mt={3}>
+                <Flex display={{ base: 'flex', md: 'flex' }} gap={5} me={'22'} mt={3}>
                     <Box as="a" href="#quranPlayer">
                         <Text _hover={{ color: 'darkgreen', cursor: "pointer" }}>Quran player</Text>
                     </Box>
@@ -25,40 +23,9 @@ const Header = () => {
                     </Box>
                 </Flex>
 
-                <IconButton
-                    bg={'gray.300'}
-                    display={{ base: 'flex', md: 'none' }}
-                    icon={<HamburgerIcon />}
-                    onClick={onOpen}
-                    me={'22'}
-                    mt={3}
-                    variant="ghost"
-                    aria-label="Open Menu"
-                />
             </Flex>
 
-            <Drawer placement="right" onClose={onClose} isOpen={isOpen}>
-                <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerCloseButton color={'black'} />
-                    <DrawerHeader color={'black'} bg={'gray.50'}>Menu</DrawerHeader>
-                    <DrawerBody bg={'gray.50'}>
-                        <VStack color={'black'} align="start" spacing={4}>
-                            <Box id="Quran player">
-                                <Text _hover={{ color: 'darkgreen', cursor: "pointer" }} onClick={onClose}>Quran player</Text>
-                            </Box>
 
-                            <Box id="Sobha">
-                                <Text _hover={{ color: 'darkgreen', cursor: "pointer" }} onClick={onClose}>Sobha</Text>
-                            </Box>
-
-                            <Box id="Prayer">
-                                <Text _hover={{ color: 'darkgreen', cursor: "pointer" }} onClick={onClose}>Prayer</Text>
-                            </Box>
-                        </VStack>
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
         </Box>
     );
 }
